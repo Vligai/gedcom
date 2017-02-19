a6b817c1ed4ffa5c2d63d0785c9ae8e2bb0b9922
Josh Gribbon <jgribbon@stevens.edu>
2017-02-19 14:52:45 -0500

    Add functions for US04 and US05, move test file to tests folder

--- Project02.py
+++ Project02.py
@@ -27,6 +27,38 @@ def less_than_150(birth, death):
     end = date.today() if death == {} else date(death["year"], death["month"], death["day"])
     return (end - birth) < 150*timedelta(days=365)
 
+def firstDateIsEarlier(d1, d2):
+    """
+    Given dates in the form {"year":1970, "month": 01, "day": 01},
+    return True if the first date is the earlier of the two
+    """
+    yyyymmdd1 = str(d1["year"]) + str(d1["month"]) + str(d1["day"])
+    yyyymmdd2 = str(d2["year"]) + str(d2["month"]) + str(d2["day"])
+    return parseInt(yyyymmdd1) < parseInt(yyyymmdd2)
+
+def marriage_before_divorce(mar, div):
+    """
+    user story 04
+    Marriage should occur before divorce of spouses,
+    and divorce can only occur after marriage
+    """
+    if div == {}:
+        return True #nothing to worry about
+    if mar == {}:
+        return False #divorce without marriage doesn't work
+    return firstDateIsEarlier(mar, div)
+
+def marriage_before_death(mar, death):
+    """
+    user story 05
+    Marriage should occur before death of either spouse
+    """
+    if mar == {}:
+        return True #nothing to worry about
+    if death == {}:
+        return True #marriage without death is fine
+    return firstDateIsEarlier(mar, death)
+
 def main(filename):
 	with open(filename, 'r') as f:
 		icurr=''
